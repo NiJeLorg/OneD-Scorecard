f0f25b11eb7b919f35e82cbc80ac36359518851a
JD Godchaux <jd@nijel.org>
2014-05-19 13:30:03 -0400

    Set new color scales on circular heat chart

--- assets/js/circularHeatChart.js
+++ assets/js/circularHeatChart.js
@@ -17,24 +17,17 @@ function nationalCircularHeatChart() {
                 .classed("circular-heat", true)
                 .attr("transform", "translate(" + parseInt(margin.left + offset) + "," + parseInt(margin.top + offset) + ")");
 
-            var autoDomain = false;
-            if (domain === null) {
-                domain = d3.extent(data, accessor);
-                autoDomain = true;
-            }
-
-            //var color = d3.scale.linear().domain(domain).range(range);
 
             g.selectAll("path").data(data)
                 .enter().append("path")
                 .attr("d", d3.svg.arc().innerRadius(ir).outerRadius(or).startAngle(sa).endAngle(ea))
                 .attr("fill", function(d, i) {
+					domain = domainFunction(i, data);
 					var color = colorFunction(i);
 					return color(accessor(d));
 				});
-
-            if(autoDomain)
-                domain = null;
+				
+			domain = null;
 
 
             // Unique id so that the text path defs are unique - is there a better way to do this?
@@ -128,6 +121,18 @@ function nationalCircularHeatChart() {
 			var color = d3.scale.linear().domain(domain).range(range);		
 		}		
 		return color;
+    }
+	
+    /* domain function */
+	// set domain based on the ring
+    function domainFunction(i, data) {
+        var domainNumber = Math.floor(i/numSegments);
+		var domainBottom = domainNumber * numSegments;
+		var domainTop = (domainNumber + 1) * numSegments;
+		// slice array to bottom and top values
+		var dataSlice = data.slice(domainBottom, domainTop);		
+		domain = d3.extent(dataSlice, accessor);				
+		return domain;
     }	
 	
     /* Configuration getters/setters */
--- protected/global.php
+++ protected/global.php
@@ -2,15 +2,15 @@
 // global.php sets the global variables for access to the One D Scorecard database
 
 // The MySQL database name
-$dbname = 'nijelorg_d3';
+$dbname = '';
 
 // The database username
-$dbuser = 'nijelorg_d3user';
+$dbuser = '';
 
 // The database host name. Don't change from "localhost" unless you have a good reason to do so.
 $dbhost = 'localhost';
 
 // The database password
-$dbpass= 'rq6BBsTq1daO';
+$dbpass= '';
 
 ?>
