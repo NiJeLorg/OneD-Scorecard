1189b7ce6d8adfd19f1d520b4ab8a2e6b463bbaa
JD Godchaux <jd@nijel.org>
2022-04-19 07:00:59 -0500

    Update setupCrosslet.js

--- js/setupCrosslet.js
+++ js/setupCrosslet.js
@@ -10,7 +10,7 @@ var dollarFormat = d3.format("$,.0f");
 var config = {
   map: {
     leaflet: {
-		url: "http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",    // backup in case stamen layer doesn't work  
+		url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",    // backup in case stamen layer doesn't work  
 		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
     },
     view: {
